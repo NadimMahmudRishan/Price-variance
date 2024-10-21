f2c5c1e99cb39cb646c71efff030e3740bb54de8
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-10-21 17:20:14 +0600

    Nav bar added with responsiveness.

---
+++ src/NavBar/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = ({nav}) => {
+  return (
+    <li className=" px-4 mb-2 py-1 border rounded-2xl md:text-xl font-bold border-red-700">
+      <a href="">{nav}</a>
+    </li>
+  );
+};
+
+export default Nav;
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,26 +1,45 @@
+import { useState } from "react";
+import { FiAlignJustify, FiX } from "react-icons/fi";
+import Nav from "./Nav";
+
 const NavBar = () => {
-    const navComponents = ['Home','Menu','Offer','Contract'];
-    return (
-      <div className="flex justify-between bg-slate-400 p-4">
-        <ul className="md:flex md:justify-around  bg-slate-400 p-4">
+  const navComponents = ["Home", "Menu", "Offer", "Contract"];
+
+  const [menu, setmenu] = useState(false);
+  const handelMenu = () => {
+    setmenu(!menu);
+  };
+  return (
+    <div className="flex justify-between bg-slate-400 p-4">
+      <div>
+        {menu ? (
+          <FiAlignJustify
+            onClick={handelMenu}
+            className=" text-2xl md:hidden"
+          />
+        ) : (
+          <FiX onClick={handelMenu} className=" text-2xl md:hidden" />
+        )}
+
+        <ul
+          className={`md:flex-row p-1 md:flex  ${
+            menu ? "-top-60": "top-14 " 
+          } flex-col md:justify-around bg-slate-400 md:static absolute rounded-2xl md:gap-5 gap-3 duration-1000 `}
+        >
           {navComponents.map((nav, xid) => (
-            <li
-              key={xid}
-              className=" m-5 px-5 py-2 border rounded-2xl text-2xl font-bold border-red-700"
-            >
-              {nav}
-            </li>
+            <Nav key={xid} nav={nav}></Nav>
           ))}
         </ul>
-        <div className=" md:w-20">
-          <img
-            className="w-20 rounded-full"
-            src="https://i.ibb.co.com/58fpDJF/IMG-20230814-133832.jpg"
-            alt="profile.png"
-          />
-        </div>
       </div>
-    );
+      <div className=" md:w-10">
+        <img
+          className="w-10 rounded-full"
+          src="https://i.ibb.co.com/58fpDJF/IMG-20230814-133832.jpg"
+          alt="profile.png"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
